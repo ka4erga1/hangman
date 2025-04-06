86fd3d2841721a3532a7eb8a9f168394ced063f7
ka4erga1 <ns.ns-ns2017@yandex.ru>
2025-04-06 23:59:15 +0300

    init

--- hangman.py
+++ hangman.py
@@ -1,8 +1,7 @@
 import random
 class hangman:
     def __init__(self):
-        self.words = ["asd", "dsa", "dgw"] # заменить на файл со словами
-        self.bukvi = ["a", "s", "d","g", "w"] # заменить на файл с буквами
+        self.bukvi = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'ё']
     def start(self):
         print("Добро пожаловать в игру виселица")
         
@@ -18,25 +17,33 @@ class hangman:
             
             
     def roung(self):
+        c = 0
+        file = open('russian_nouns.txt', 'r', encoding='utf-8')
+        content = file.readlines()
+        while c <= 5:
+            self.word = list(content[random.randint(1, 51301)])
+            self.word.pop(len(self.word)-1)
+            c = len(self.word)
+        file.close()
+        print(self.word)
         self.guessed = []
-        self.word = []
-        self.word = list(self.words[random.randint(0, 2)])
         for i in self.word:
             self.guessed.append("_")
         game.turn()
             
     def turn(self):
         self.lives = 10
-        self.wrong = []
+        self.wrongs = []
         while self.lives != 0:
             if self.word == self.guessed:
+                
                 break
             print("Выбери букву:")
             f = input().lower()
             if f in self.guessed:
                 print("Буква повторяется")
             elif f in self.bukvi and f in self.word:
-                game.right(self, f)
+                game.right(f)
             elif f in self.bukvi and f not in self.word:
                 game.wrong(f)
             else:
@@ -45,19 +52,20 @@ class hangman:
     
     def right(self, f):
         print("Буква ", f, " есть")
-        for i in range(self.word.count(f)):
-            self.guessed.pop(self.word.index(f))
-            self.guessed.insert(self.word.index(f), f)
+        for i in range(len(self.word)):
+            if self.word[i] == f:
+                self.guessed.pop(i)
+                self.guessed.insert(i, f)
         print(game.prins())
         print(self.guessed)
         
     def wrong(self, f):
         print("Буквы ", f, " нет")
         self.lives -= 1
-        self.wrong.append(f)
+        self.wrongs.append(f)
         print(game.prins())
         print("Угаданные: ", self.guessed)
-        print("Неверные: ", self.wrong)
+        print("Неверные: ", self.wrongs)
      
     def prins(self):
         s = "*Сделать виселицу"
@@ -65,8 +73,10 @@ class hangman:
     
     def end(self):
         if self.lives > 0:
+            print("Слово: ", self.word)
             print("Ты победил!")
         else:
+            print("Слово: ", self.word)
             print("Тебя повесили")
         c = input("Сыграем еще? (Да/Нет)").lower()
         if c == "да":
